9f5ef1e738d29adcde5eb921685f45737fd48364
mangala-karunarathne <mangalaicc@gmail.com>
2023-05-13 07:52:39 +0530

    CHG | Custom Middleware for Error Handling

--- server.js
+++ server.js
@@ -5,10 +5,28 @@ const port = 5000;
 const apiRoutes = require("./routes/apiRoutes")
 
 app.get("/", (req, res) => {
+  console.log("synchronous code");
+  throw new Error("some error occured")
   res.json({message: "API running..."})
 });
 
+app.get('/a', (req, res, next)=> {
+  setTimeout(()=>{
+    try {
+      console.log("asynchronous code");
+    } catch (error) {
+      next(error)
+    }
+  },1000)
+
+})
+
 app.use('/api', apiRoutes)
+
+app.use((error, req, res, next)=>{
+  console.error(error);
+  next(error)
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
