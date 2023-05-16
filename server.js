c36744d8475ec2317a0efdd7cba15064067f8be7
mangala-karunarathne <mangalaicc@gmail.com>
2023-05-16 19:48:51 +0530

    CHG | Product Model

--- server.js
+++ server.js
@@ -4,25 +4,12 @@ const port = 5000;
 
 const apiRoutes = require("./routes/apiRoutes");
 
-app.get("/", async(req, res, next) => {
-  const Product = require("./models/ProductModel")
-  try {
-    const product = new Product
-    product.name = "New Product Name 2"
-    const productSaved = await product.save()
-    console.log(productSaved === product);
-    const products = await Product.find()
-    console.log(products.length);
-    res.send("Product Created " + product._id)
-  } catch (error) {
-    next(error)
-  }
-  // res.json({ message: "API running..." });
+app.get("/", async (req, res, next) => {
+  res.json({ message: "API running..." });
 });
 
-
 // Mongodb connection
-const connectDB = require("./config/db")
+const connectDB = require("./config/db");
 connectDB();
 
 app.use("/api", apiRoutes);
@@ -34,10 +21,10 @@ app.use((error, req, res, next) => {
 
 app.use((error, req, res, next) => {
   res.status(500).json({
-      message: error.message,
-      stack: error.stack
-  })
-})
+    message: error.message,
+    stack: error.stack,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
