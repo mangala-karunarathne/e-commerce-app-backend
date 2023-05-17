6de0b56168cf107347e21b11ca61c97c07205c48
mangala-karunarathne <mangalaicc@gmail.com>
2023-05-17 19:23:27 +0530

    CHG | User Model

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -1,59 +1,66 @@
 const mongoose = require("mongoose");
 const Review = require("./ReviewModel");
 const imageSchema = mongoose.Schema({
-  path: {type: String, required: true}
+  path: { type: String, required: true },
 });
 
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      uni: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    count: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Number,
+    },
+    reviewsNumber: {
+      type: Number,
+    },
+    sales: {
+      type: Number,
+      default: 0,
+    },
+    attrs: [
+      [
+        { key: { type: String }, value: { type: String } },
+        // {key: "color", value:"red"}, {key:"size", value:"1 TB"}
+      ],
+    ],
+    images: [imageSchema],
+    reviews: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: Review,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+const Product = mongoose.model("Product", productSchema);
+productSchema.index(
+  { name: "text", description: "text" },
+  { name: "TextIndex" }
+);
+productSchema.index({ "attrs.key": 1, "attrs.value": 1 }),
+  // productSchema.index({name:-1}),   // For Descending order of sorting in DB when searching
 
-const productSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    uni: true,
-  },
-  description:{
-    type: String,
-    required: true,
-  },
-  category:{
-    type: String,
-    required: true,
-  },
-  count:{
-    type: Number,
-    required: true,
-  },
-  price:{
-    type: Number,
-    required: true,
-  },
-  rating:{
-    type: Number,
-  },
-  reviewsNumber:{
-    type: Number,
-  },
-  sales:{
-    type: Number,
-    default:0,
-  },
-  attrs: [
-    [
-      {key:{type:String}, value:{type:String}}
-      // {key: "color", value:"red"}, {key:"size", value:"1 TB"}
-    ]
-  ],
-  images:[imageSchema],
-  reviews:[{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: Review,
-  }]
-},{
-  timestamps: true,
-});
-const Product = mongoose.model("Product", productSchema)
-productSchema.index({name:"text", description:"text"},{name:"TextIndex"})
-productSchema.index({"attrs.key":1, "attrs.value":1}),
-// productSchema.index({name:-1}),   // For Descending order of sorting in DB when searching
-
-module.exports = Product
+  (module.exports = Product);
