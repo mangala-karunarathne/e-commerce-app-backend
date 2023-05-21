91794f2059101540b33560cc7c47c6456ab3c713
mangala-karunarathne <mangalaicc@gmail.com>
2023-05-21 17:11:44 +0530

    CHG | API for Create New Category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,12 +1,35 @@
-const Category = require("../models/CategoryModel")
+const Category = require("../models/CategoryModel");
 
-const getCategories = async(req, res, next) => {
-   try {
-    const categories = await Category.find().sort({name:"asc"}).orFail()
-    res.json(categories)
-   } catch (error) {
-    next(error)
-   }
-}  
+const getCategories = async (req, res, next) => {
+  try {
+    const categories = await Category.find().sort({ name: "asc" }).orFail();
+    res.json(categories);
+  } catch (error) {
+    next(error);
+  }
+};
 
-module.exports = getCategories
+const newCategory = async (req, res, next) => {
+  try {
+    const {newCategory} = req.body
+    if(!newCategory){
+        res.status(400).send("Category input is required")
+    }
+
+const categoryExists = await Category.findOne({name: newCategory})
+
+if(categoryExists){
+    res.status(400).send("Category already exists")
+}else{
+    const categoryCreated = await Category.create({
+        name: newCategory
+    })
+    res.status(201).send({categoryCreated: categoryCreated})
+}
+    res.send(newCategory)
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getCategories, newCategory };
