407566cd3b58cbf5866399764ae3a1d37a4863f3
mangala-karunarathne <mangalaicc@gmail.com>
2023-05-22 07:37:57 +0530

    CHG | Category Delete API

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -11,25 +11,40 @@ const getCategories = async (req, res, next) => {
 
 const newCategory = async (req, res, next) => {
   try {
-    const {newCategory} = req.body
-    if(!newCategory){
-        res.status(400).send("Category input is required")
+    const { newCategory } = req.body;
+    if (!newCategory) {
+      res.status(400).send("Category input is required");
     }
 
-const categoryExists = await Category.findOne({name: newCategory})
+    const categoryExists = await Category.findOne({ name: newCategory });
 
-if(categoryExists){
-    res.status(400).send("Category already exists")
-}else{
-    const categoryCreated = await Category.create({
-        name: newCategory
-    })
-    res.status(201).send({categoryCreated: categoryCreated})
-}
-    res.send(newCategory)
+    if (categoryExists) {
+      res.status(400).send("Category already exists");
+    } else {
+      const categoryCreated = await Category.create({
+        name: newCategory,
+      });
+      res.status(201).send({ categoryCreated: categoryCreated });
+    }
+    res.send(newCategory);
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = { getCategories, newCategory };
+const deleteCategory = async (req, res, next) => {
+  // return res.send(req.params.category)
+  try {
+    if (req.params.category !== "Choose Category") {
+      const categoryExists = await Category.findOne({
+        name: decodeURIComponent(req.params.category),
+      }).orFail();
+      await categoryExists.remove();
+      res.json({ categoryDeleted: true });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getCategories, newCategory, deleteCategory };
