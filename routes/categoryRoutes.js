4e2f2ca4aa6068f98a6cdc3424247e735204086e
mangala-karunarathne <mangalaicc@gmail.com>
2023-05-29 22:39:28 +0530

    CHG | Save Attribute API Part I

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express() 
-const {getCategories, newCategory, deleteCategory} = require("../controllers/categoryController")
+const {getCategories, newCategory, deleteCategory, saveAtr} = require("../controllers/categoryController")
 
 router.get("/", getCategories)
 router.post("/", newCategory)
 router.delete("/:category", deleteCategory)
+router.post("/attr", saveAtr)
 
 module.exports = router
