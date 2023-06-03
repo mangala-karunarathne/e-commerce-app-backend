61df5e33bbc34c94ffb2d809d83606cab806fa04
mangala-karunarathne <mangalaicc@gmail.com>
2023-06-03 21:24:20 +0530

    CHG | Getting Products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,13 @@
+const recordsPerPage = require("../config/pagination")
 const Product = require("../models/ProductModel")
 
-const getProducts = (req, res) => {
-   
+const getProducts = async(req, res, next) => {
+   try {
+    const products = await Product.find({}).sort({name: 1}).limit(recordsPerPage)
+    res.json({products})
+   } catch (error) {
+    
+   }
     res.send(" Handling Product Routes")
 }  
 
