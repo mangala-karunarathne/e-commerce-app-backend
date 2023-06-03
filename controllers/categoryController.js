d13468dd9d335081aeeb2e7b4cf602fd5753c83d
mangala-karunarathne <mangalaicc@gmail.com>
2023-06-03 19:53:15 +0530

    FIX | Add Attribute & Value when create a new Product

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -57,36 +57,40 @@ const deleteCategory = async (req, res, next) => {
   }
 };
 
-const saveAtr = async (req, res, next) => {
-  const { key, val, categoryChoosen } = req.body;
-
-  if (!key || !val || !categoryChoosen) {
-    return res.status(400).send("All inputs are required");
+const saveAttr = async (req, res, next) => {
+  const {key, val, categoryChoosen} = req.body
+  if(!key || !val || !categoryChoosen) {
+      return res.status(400).send("All inputs are required")
   }
   try {
-    const category = categoryChoosen.split("/")[0];
-    const categoryExists = await Category.findOne({ name: category }).orFail();
-    if (categoryExists.attrs.length > 0) {
-      // if key exists in the db then add a value to the key
-      var keyDoesNotExistsInDatabase = true;
+      const category = categoryChoosen.split("/")[0]
+      const categoryExists = await Category.findOne({name: category}).orFail()
+      if(categoryExists.attrs.length > 0) {
+          // if key exists in the database then add a value to the key
+          var keyDoesNotExistsInDatabase = true
+          categoryExists.attrs.map((item, idx) => {
+              if(item.key === key) {
+                  keyDoesNotExistsInDatabase = false
+                  var copyAttributeValues = [...categoryExists.attrs[idx].value]
+                  copyAttributeValues.push(val)
+                  var newAttributeValues = [...new Set(copyAttributeValues)] // Set ensures unique values
+                  categoryExists.attrs[idx].value = newAttributeValues
+              }
+          })
 
-      categoryExists.attrs.map((item, idx) => {
-        if (item.key === key) {
-          keyDoesNotExistsInDatabase = false
-          var copyAttributeValues = [...categoryExists.attrs[idx].value]
-        }
-      });
-
-      if (keyDoesNotExistsInDatabase) {
-        categoryExists.attrs.push({ key: key, value: [val] });
+          if(keyDoesNotExistsInDatabase) {
+              categoryExists.attrs.push({key: key, value: [val]})
+          }
+      } else {
+          // push to the array
+          categoryExists.attrs.push({key: key, value: [val]})
       }
-    } else {
-      // push to the array
-      categoryExists.attrs.push({ key: key, value: [val] });
-    }
-  } catch (error) {
-    next(error);
+      await categoryExists.save()
+      let cat = await Category.find({}).sort({name: "asc"})
+      return res.status(201).json({categoriesUpdated: cat})
+  } catch(err) {
+      next(err)
   }
-};
+}
 
-module.exports = { getCategories, newCategory, deleteCategory, saveAtr };
+module.exports = { getCategories, newCategory, deleteCategory, saveAttr };
