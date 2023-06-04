b849d3fbafcdcbf79f7b9afa3cd3796160c8f888
mangala-karunarathne <mangalaicc@gmail.com>
2023-06-04 08:33:27 +0530

    CHG | Sort Products

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,9 +5,20 @@ const getProducts = async (req, res, next) => {
   try {
     const pageNum = Number(req.query.pageNum) || 2;
     const totalProducts = await Product.countDocuments({});
+
+    // Sort by name, price etc.
+    let sort = {};
+    const sortOption = req.query.sort || ""
+
+    if(sortOption){
+        let sortOpt = sortOption.split("_")
+        console.log(sortOpt);
+        sort = { [sortOpt[0]]: Number(sortOpt[1])}
+    }
+
     const products = await Product.find({})
       .skip(recordsPerPage * (pageNum - 1))
-      .sort({ name: 1 })
+      .sort(sort)
       .limit(recordsPerPage);
     res.json({
       products,
