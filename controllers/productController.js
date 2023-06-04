4e7ebbdf9b143137c7f23d5da0a64303de622355
mangala-karunarathne <mangalaicc@gmail.com>
2023-06-04 11:02:48 +0530

    CHG | Filter Results

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,20 +3,46 @@ const Product = require("../models/ProductModel");
 
 const getProducts = async (req, res, next) => {
   try {
+    let query = {};
+
+    let queryCondition = false;
+
+    let priceQueryCondition = {};
+
+    if (req.query.price) {
+      queryCondition = true;
+      priceQueryCondition = { price: { $lte: Number(req.query.price) } };
+    }
+
+    let ratingQueryCondition = {};
+
+    if (req.query.rating) {
+      queryCondition = true;
+      const ratingArray = req.query.rating.split(",").map(Number);
+      ratingQueryCondition = { rating: { $in: ratingArray } };
+    }
+
+    if (queryCondition) {
+      query = {
+        $and: [priceQueryCondition, ratingQueryCondition],
+      };
+    }
+
+    // Pagination
     const pageNum = Number(req.query.pageNum) || 2;
-    const totalProducts = await Product.countDocuments({});
 
     // Sort by name, price etc.
     let sort = {};
-    const sortOption = req.query.sort || ""
+    const sortOption = req.query.sort || "";
 
-    if(sortOption){
-        let sortOpt = sortOption.split("_")
-        console.log(sortOpt);
-        sort = { [sortOpt[0]]: Number(sortOpt[1])}
+    if (sortOption) {
+      let sortOpt = sortOption.split("_");
+      // console.log(sortOpt);
+      sort = { [sortOpt[0]]: Number(sortOpt[1]) };
     }
 
-    const products = await Product.find({})
+    const totalProducts = await Product.countDocuments(query);
+    const products = await Product.find(query)
       .skip(recordsPerPage * (pageNum - 1))
       .sort(sort)
       .limit(recordsPerPage);
