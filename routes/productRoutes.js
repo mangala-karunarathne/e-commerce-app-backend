451cdd76faa6d32b6949948b29c7dac0294c699f
mangala-karunarathne <mangalaicc@gmail.com>
2023-06-05 23:26:34 +0530

    CHG | Get Prodcuts From Specific Category

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express()
 const getProducts = require("../controllers/productController")
 
+router.get("/category/:categoryName", getProducts)
 router.get("/", getProducts)
 
 module.exports = router
