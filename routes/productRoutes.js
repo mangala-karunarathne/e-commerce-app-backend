6bfba49ae518627266bad6949f8f75c1929745ba
mangala-karunarathne <mangalaicc@gmail.com>
2023-06-09 22:18:09 +0530

    CHG | Query for Searching Products Through Search Box-I

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,9 @@ const express = require("express")
 const router = express()
 const getProducts = require("../controllers/productController")
 
+router.get("/category/:categoryName/search/:searchQuery", getProducts)
 router.get("/category/:categoryName", getProducts)
+router.get("/search/:searchQuery", getProducts)
 router.get("/", getProducts)
 
 module.exports = router
