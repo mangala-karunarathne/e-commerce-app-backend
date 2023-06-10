dc0fe23979eeda785b1926e9686eeddffc5e4fae
mangala-karunarathne <mangalaicc@gmail.com>
2023-06-10 13:48:55 +0530

    CHG | Get Product By Id with Reviews

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const router = express()
-const getProducts = require("../controllers/productController")
+const {getProducts, getProductById} = require("../controllers/productController")
 
 router.get("/category/:categoryName/search/:searchQuery", getProducts)
 router.get("/category/:categoryName", getProducts)
 router.get("/search/:searchQuery", getProducts)
 router.get("/", getProducts)
+router.get("/:id", getProductById)
 
 module.exports = router
