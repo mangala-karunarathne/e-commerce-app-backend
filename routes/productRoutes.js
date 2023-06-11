371607267d84b23486745dfa04bb1b3cfd1ca83a
mangala-karunarathne <mangalaicc@gmail.com>
2023-06-11 12:19:53 +0530

    CHG | Create Product By Admin

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express()
-const {getProducts, getProductById, getBestSellers, adminGetProduct, adminDeleteProduct} = require("../controllers/productController")
+
+const {getProducts, getProductById, getBestSellers, adminGetProduct, adminDeleteProduct, adminCreateProduct } = require("../controllers/productController")
 
 router.get("/category/:categoryName/search/:searchQuery", getProducts)
 router.get("/category/:categoryName", getProducts)
@@ -12,6 +13,6 @@ router.get("/get-one/:id", getProductById)
 // admin routes
 router.get("/admin", adminGetProduct)
 router.delete("/admin/:id", adminDeleteProduct)
-
+router.post("/admin", adminCreateProduct)
 
 module.exports = router
