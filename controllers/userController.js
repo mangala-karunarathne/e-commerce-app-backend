9bfe3ec3cba347930a1682596b4f1cdf12b4c080
mangala-karunarathne <mangalaicc@gmail.com>
2023-06-15 22:21:11 +0530

    CHG | Register a User

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,4 +9,28 @@ const getUsers = async (req, res, next) => {
   }
 };
 
-module.exports = getUsers;
+const registerUser = async (req, res, next) => {
+  try {
+    const { name, lastName, email, password } = req.body;
+    if (!(name && lastName && email && password)) {
+      return res.status(400).send("All inputs are required");
+    }
+
+    const userExists = await User.findOne({ email });
+    if(userExists){
+      return res.status(400).json({error: "User already exists"})
+    }else{
+      const user = await User.create({
+        name,
+        lastName,
+        email: email.toLowerCase(),
+        password: password
+      })
+      res.status(201).send(user)
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {getUsers, registerUser};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,12 @@
-const express = require("express")
-const router = express()
-const getUsers = require("../controllers/userController.js")
+const express = require("express");
+const router = express();
+const { getUsers, registerUser } = require("../controllers/userController.js");
 
-router.get("/", getUsers)
+router.get("/", getUsers);
 
-module.exports = router
+// user logged in routes:
+
+// admin routes:
+router.post("/register", registerUser);
+
+module.exports = router;
