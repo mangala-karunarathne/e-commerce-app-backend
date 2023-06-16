8014cda908352c8bbc3eb5d12f6abc1d660b2f70
mangala-karunarathne <mangalaicc@gmail.com>
2023-06-16 22:01:37 +0530

    CHG | Add Hash Password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/UserModel");
+const { hashPassword } = require("../utils/hashPassword");
 
 const getUsers = async (req, res, next) => {
   try {
@@ -15,22 +16,22 @@ const registerUser = async (req, res, next) => {
     if (!(name && lastName && email && password)) {
       return res.status(400).send("All inputs are required");
     }
-
+    const hashedPassword = hashPassword(password);
     const userExists = await User.findOne({ email });
-    if(userExists){
-      return res.status(400).json({error: "User already exists"})
-    }else{
+    if (userExists) {
+      return res.status(400).json({ error: "User already exists" });
+    } else {
       const user = await User.create({
         name,
         lastName,
         email: email.toLowerCase(),
-        password: password
-      })
-      res.status(201).send(user)
+        password: hashPassword,
+      });
+      res.status(201).send(user);
     }
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = {getUsers, registerUser};
+module.exports = { getUsers, registerUser };
