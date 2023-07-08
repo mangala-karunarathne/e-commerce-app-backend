657c14a6ca3d12a3b83cc3b5ba5d8f9348aa1385
mangala-karunarathne <mangalaicc@gmail.com>
2023-07-08 10:59:05 +0530

    CHG | Add cors to the backend server

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
 app.use(express.json());
 app.use(fileUpload());
 app.use(cookieParser());
+app.use(cors());
 
 const apiRoutes = require("./routes/apiRoutes");
 
